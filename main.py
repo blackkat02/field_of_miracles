bebb272bafbe00fce0ce1ec154d5210bb4894726
blackkat02 <138970349+blackkat02@users.noreply.github.com>
2023-08-04 13:36:02 +0300

    Changed the value of word1 from a list to a string. attempt_number moved to responsibility blocks for false attempts

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import random
 word = random.choice(text)
 word = word.lower()
 length_word = len(word)
-word1 = []
+word1 = ""
 for i in word:
-    word1.append("*")
-print(word1)
+    i = "*"
+    word1 = word1 + i
+print(word, type(word), word1, type(word1))
 number_of_attempts = input("Enter number of attempts word: ")
 
 if number_of_attempts.isdigit():
@@ -17,20 +18,21 @@ if number_of_attempts.isdigit():
             attempt = input("Enter word or letter : ")
             attempt = attempt.lower()
             length_attempt = len(attempt)
-            attempt_number += 1
             if length_attempt > 1:
                 if attempt == word:
                     print("Congratulations, you guessed the word")
                     exit()
                 else:
+                    attempt_number += 1
                     print(f"The word is not correct, {number_of_attempts - attempt_number} attempts left")
             elif length_attempt == 1:
                 if attempt in word:
                     for i in range(0, length_word):
                         if attempt == word[i]:
-                            word1[i] = word[i]
+                            word1 = word1[:i] + word[i] + word1[i+1:]
                             print(f"{word1}, {number_of_attempts - attempt_number} attempts left")
                 else:
+                    attempt_number += 1
                     print(f"There is no letter, {number_of_attempts - attempt_number} attempts left")
 
     print("You lose, game over , 0 attempts left")
